db0e9e6c49be30e22ac4265b22b8be6a8a27d2f5
pallav12 <rajeevpallav606@gmail.com>
2019-12-11 11:54:39 +0530

    attemt to merge

---
+++ app/src/main/java/org/kashiyatra/ky19/inner/InnerAdapter.java
@@ -0,0 +1,58 @@
+package org.kashiyatra.ky19.inner;
+
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+
+
+import org.kashiyatra.ky19.R;
+import org.kashiyatra.ky19.databinding.InnerItemBinding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class InnerAdapter extends com.ramotion.garlandview.inner.InnerAdapter<InnerItem> {
+
+    private final List<InnerData> mData = new ArrayList<>();
+
+    @Override
+    public InnerItem onCreateViewHolder(ViewGroup parent, int viewType) {
+        final InnerItemBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), viewType, parent, false);
+        return new InnerItem(binding.getRoot());
+    }
+
+    @Override
+    public void onBindViewHolder(InnerItem holder, int position) {
+        holder.setContent(mData.get(position));
+    }
+
+    @Override
+    public void onViewRecycled(InnerItem holder) {
+        holder.clearContent();
+    }
+
+    @Override
+    public int getItemCount() {
+        return mData.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return R.layout.inner_item;
+    }
+
+    public void addData(@NonNull List<InnerData> innerDataList) {
+        final int size = mData.size();
+        mData.addAll(innerDataList);
+        notifyItemRangeInserted(size, innerDataList.size());
+    }
+
+    public void clearData() {
+        mData.clear();
+        notifyDataSetChanged();
+    }
+
+}
---
+++ app/src/main/java/org/kashiyatra/ky19/utils/FontBinder.java
@@ -0,0 +1,19 @@
+package org.kashiyatra.ky19.utils;
+
+
+import android.content.res.AssetManager;
+import android.graphics.Typeface;
+import android.widget.TextView;
+
+import androidx.databinding.BindingAdapter;
+
+
+public class FontBinder {
+
+    @BindingAdapter("bind:font")
+    public static void setTypeface(TextView textView, String fontName) {
+        final AssetManager assets = textView.getContext().getAssets();
+        textView.setTypeface(Typeface.createFromAsset(assets, "fonts/" + fontName));
+    }
+
+}
